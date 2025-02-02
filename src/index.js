62ec80ec69c05383622d8febb04c86e0b38e3cfb
PryCoder <priyanshugupta007007@gmail.com>
2025-02-02 16:32:51 +0530

    Creating utils for API res,err and async Handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import dotenv from "dotenv";
+import { app } from "./app.js";
 
 dotenv.config({
     path: "./.env"
@@ -8,4 +9,11 @@ dotenv.config({
 
 import connectDB from "./db/index.js";
 
-connectDB();
+connectDB()
+.then((() => {
+    app.listen(process.env.PORT || 8000, () => {console.log(`Server started t0 ${process.env.PORT}`)
+})
+}))
+.catch((err) => {
+    console.log("MONGODB connection failed",err);
+})
