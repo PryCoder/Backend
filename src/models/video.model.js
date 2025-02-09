f2be34f75f054eff75ca88d0889c40bf750f60c8
PryCoder <priyanshugupta007007@gmail.com>
2025-02-09 23:41:22 +0530

    creating models

---
+++ src/models/video.model.js
@@ -0,0 +1,45 @@
+import mongoose,{Schema}from "mongoose"
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new mongoose.Schema(
+    {
+     videoFile:{
+        type:String,
+        required:true,
+     },
+     thumbnail:{
+        type:String,
+        required: true
+     },
+     title:{
+        type:String,
+        required:true,
+
+     },
+     description:{
+        type:String,
+        requiered :true
+     },
+     duration :{
+        type: String,
+        required:true,
+
+     },
+     views:{
+        type:String,
+        required:true,
+
+     },
+     isPublished:{
+        type :String,
+        required:true,
+     },owner:{
+        type:Schema.Types.ObjectId,
+        ref:"User"
+     }
+},{timestamps:true}
+)
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema)
